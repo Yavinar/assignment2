42765a77f1694246e98c1a161b8df50f964dd1bb
Yavinar <120156505+Yavinar@users.noreply.github.com>
2023-01-13 10:57:37 +0530

    Update ques2.py

--- ques2.py
+++ ques2.py
@@ -4,9 +4,9 @@ Hey, ABC Here!
 My SID is 2110XXXX
 I am from XYZ department and my CGPA is 9.9"""
 
-name="Siddhant Arora"
-SID=22106052
-dept="Data Science"
+name="Yavinar Kumar"
+SID=22104124
+dept="EE"
 cgpa=9.9
 print("Hey,",name,"here!","\nMy SID is",SID,"\nI am from",dept,"and my cgpa is",cgpa)
 
